b65ab80b01d8cfe00664c552e6abd21df63e0c5f
AnastasiaGer <aagermanova@gmail.com>
2021-01-14 20:04:56 -0800

    array

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const message = 'Next';
 
-const helloMessage = 'Hello again'
+var carYear = 2000
+var personYear = 1990
 
-console.log(message, helloMessage)
+function calculateAge(year) {
+  var currentYear = 2020
+  var result = currentYear - year
+  return result
+}
+
+function checkAngLogAge(year, name, compareTo) {
+  if (calculateAge(year) < compareTo) {
+    console.log('Возраст ' + name + ' меньше ' + compareTo + ' лет')
+  } else {
+    console.log('Возраст '  + name + ' больше ' + compareTo + ' лет')
+  }
+}
+
+checkAngLogAge(carYear, 'машины', 8)
+checkAngLogAge(personYear, 'человека', 40)
+
+
+const fruits = ['mangoes', 'apples', 'bananas']
+const b = fruits.pop();
+console.log(b)
