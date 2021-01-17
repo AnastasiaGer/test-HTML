7c3d75f2d803ed21f3054bfd417139e04ee51948
AnastasiaGer <aagermanova@gmail.com>
2021-01-16 20:41:15 -0800

    еуые

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-// class -> .CLASS_NAME
-// id -> #ID
-// tag -> TAG_NAME
+document.querySelector('button').addEventListener('click', function(event) {
+  const value = document.querySelector('input').value
 
-var a = document.querySelector('a')
-var oldHref = a.getAttribute('href')
+  const obj = {
+    text : value
+  }
+  localStorage.setItem('header', JSON.stringify(obj))
+})
 
-a.setAttribute('href', 'https://ya.ru')
-a.setAttribute('title', 'Go to yandex')
-a.textContent = 'Yandex'
+document.addEventListener('DOMContentLoaded', function() {
+  let obj;
+  try {
+    obj = JSON.parse(localStorage.getItem('header'))
+  } catch (e) {
+    obj = {}
+  }
 
-console.log(a.attributes)
+  if (obj.text && obj.text.trim()) {
+    document.querySelector('h1').textContent = obj.text
+  }
 
-
-const box1 = document.querySelector('#box1')
-const box2 = document.querySelector('#box2')
-
-box1.classList.add('red')
-
-box2.classList.add('blue')
+})
