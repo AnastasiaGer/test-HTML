1f4be7c6b9988d70dd7a8279e2a5f23b696064ef
AnastasiaGer <aagermanova@gmail.com>
2021-01-17 14:43:20 -0800

    game4

--- script.js
+++ script.js
@@ -4,21 +4,32 @@ const $time = document.querySelector('#time')
 const $timeHeader = document.querySelector('#time-header')
 const $resultHeader = document.querySelector('#result-header')
 const $result = document.querySelector('#result')
+const $gameTime = document.querySelector('#game-time')
+
+const colors =['red', 'blue', 'green', 'yellow', 'pink', 'black', 'orange', 'purple']
 
 let score = 0
 let isGameStarted = false
 
+function show($el) {
+  $el.classList.remove('hide')
+}
+
+function hide($el) {
+  $el.classList.add('hide')
+}
+
 $start.addEventListener('click', startGame)
 $game.addEventListener('click', handelBoxClick)
+$gameTime.addEventListener('input', setGameTime)
 
 function startGame() {
   score = 0
   setGameTime()
-  $timeHeader.classList.remove('hide')
-  $resultHeader.classList.add('hide')
+  $gameTime.setAttribute('disabled', 'true')
   isGameStarted = true
   $game.style.backgroundColor = '#fff'
-  $start.classList.add('hide')
+  hide($start)
   const interval = setInterval(function () {
     let time = parseFloat($time.textContent)
     if (time <= 0) {
@@ -37,18 +48,22 @@ function setGameScore() {
 }
 
 function setGameTime() {
-  const time = 5
+  const time = +$gameTime.value
   $time.textContent = time.toFixed(1)
+  show($timeHeader)
+  hide($resultHeader)
 }
 
 function endGame() {
   isGameStarted = false
-  $start.classList.remove('hide')
+  setGameScore()
+  $gameTime.removeAttribute('disabled')
+  show($start)
   $game.innerHTML = ''
   $game.style.backgroundColor = '#ccc'
-  $timeHeader.classList.add('hide')
-  $resultHeader.classList.remove('hide')
-  setGameScore()
+  hide($timeHeader)
+  show($resultHeader)
+
 }
 
 function handelBoxClick(event) {
@@ -68,10 +83,11 @@ function renderBox() {
   const gameSize = $game.getBoundingClientRect()
   const maxTop = gameSize.height - boxSize
   const maxLeft = gameSize.width - boxSize
+  const randomColorsIndex = getRandom(0, colors.length - 1)
 
   box.style.height = box.style.width = boxSize + 'px'
   box.style.position = 'absolute'
-  box.style.backgroundColor = '#000'
+  box.style.backgroundColor = colors[randomColorsIndex]
   box.style.top = getRandom(0, maxTop) + 'px'
   box.style.left = getRandom(0, maxLeft) + 'px'
   box.style.cursor = 'pointer'
