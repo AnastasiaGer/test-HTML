1820aa0e59c2c8e00bd62f6b8ca61444eee7957d
AnastasiaGer <aagermanova@gmail.com>
2021-01-18 13:51:21 -0800

    proto

--- script.js
+++ script.js
@@ -1,101 +1,17 @@
-const $start = document.querySelector('#start')
-const $game = document.querySelector('#game')
-const $time = document.querySelector('#time')
-const $timeHeader = document.querySelector('#time-header')
-const $resultHeader = document.querySelector('#result-header')
-const $result = document.querySelector('#result')
-const $gameTime = document.querySelector('#game-time')
-
-const colors =['red', 'blue', 'green', 'yellow', 'pink', 'black', 'orange', 'purple']
-
-let score = 0
-let isGameStarted = false
-
-function show($el) {
-  $el.classList.remove('hide')
+function Car(name, year) {
+  this.name = name
+  this.year = year
 }
 
-function hide($el) {
-  $el.classList.add('hide')
+Car.prototype.getAge = function() {
+  return new Date().getFullYear() - this.year
 }
 
-$start.addEventListener('click', startGame)
-$game.addEventListener('click', handelBoxClick)
-$gameTime.addEventListener('input', setGameTime)
+Car.prototype.color = 'black'
 
-function startGame() {
-  score = 0
-  setGameTime()
-  $gameTime.setAttribute('disabled', 'true')
-  isGameStarted = true
-  $game.style.backgroundColor = '#fff'
-  hide($start)
-  const interval = setInterval(function () {
-    let time = parseFloat($time.textContent)
-    if (time <= 0) {
-      clearInterval(interval)
-      endGame()
-    } else {
-      $time.textContent = (time - 0.1).toFixed(1)
-    }
-  }, 100)
+var ford = new Car('Ford', 2015)
+var bmw = new Car('BMW', 2017)
 
-  renderBox()
-}
-
-function setGameScore() {
-  $result.textContent = score.toString()
-}
-
-function setGameTime() {
-  const time = +$gameTime.value
-  $time.textContent = time.toFixed(1)
-  show($timeHeader)
-  hide($resultHeader)
-}
-
-function endGame() {
-  isGameStarted = false
-  setGameScore()
-  $gameTime.removeAttribute('disabled')
-  show($start)
-  $game.innerHTML = ''
-  $game.style.backgroundColor = '#ccc'
-  hide($timeHeader)
-  show($resultHeader)
-
-}
-
-function handelBoxClick(event) {
-  if (!isGameStarted) {
-    return
-  }
-  if (event.target.dataset.box) {
-    score++
-    renderBox()
-  }
-}
-
-function renderBox() {
-  $game.innerHTML = ''
-  let box = document.createElement('div')
-  const boxSize = getRandom(30, 100)
-  const gameSize = $game.getBoundingClientRect()
-  const maxTop = gameSize.height - boxSize
-  const maxLeft = gameSize.width - boxSize
-  const randomColorsIndex = getRandom(0, colors.length - 1)
-
-  box.style.height = box.style.width = boxSize + 'px'
-  box.style.position = 'absolute'
-  box.style.backgroundColor = colors[randomColorsIndex]
-  box.style.top = getRandom(0, maxTop) + 'px'
-  box.style.left = getRandom(0, maxLeft) + 'px'
-  box.style.cursor = 'pointer'
-  box.setAttribute('data-box', 'true')
-
-  $game.insertAdjacentElement('afterbegin', box)
-}
-
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min) + min)
-}
+ford.color = 'red'
+console.log(ford)
+console.log(bmw)
