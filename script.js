f5e5b7e83bb9e5963bb48f2cf6711b97973b71f4
AnastasiaGer <aagermanova@gmail.com>
2021-01-18 14:43:05 -0800

    callback

--- script.js
+++ script.js
@@ -1,53 +1,25 @@
-var ford = Object.create({
-  calculateDistancePerYear: function() {
-    Object.defineProperty(this, 'distancePerYear', {
-      value: Math.ceil(this.distance / this.age),
-      enumerable: false,
-      writable: false,
-      configurable: false
-    })
-  }
-}, {
-  name: {
-    value: 'Ford',
-    enumerable: true,
-    writable: false,
-    configurable: false
-  },
-  model: {
-    value: 'Focus',
-    enumerable: true,
-    writable: false,
-    configurable: false
-  },
-  year: {
-    value: 2015,
-    enumerable: true,
-    writable: false,
-    configurable: false
-  },
-  distance: {
-    value: 120500,
-    enumerable: true,
-    writable: true,
-    configurable: false
-  },
-  age: {
-    enumerable: true,
-    get: function() {
-      console.log('Получаем возраст')
-      return new Date().getFullYear() - this.year
+var createCounter = function(counterName) {
+  var counter = 0
+
+  return {
+    increment: function() {
+      counter++
     },
-    set: function() {
-      console.log('Устанавливаем значение')
+    decrement: function() {
+      counter--
+    },
+    getCounter: function() {
+      return counter
     }
   }
-})
-
-ford.calculateDistancePerYear()
-
-for (var key in ford) {
-  if (ford.hasOwnProperty(key)) {
-    console.log(key, ford[key])
-  }
 }
+
+var counterA = createCounter('Counter A')
+var counterB = createCounter('Counter B')
+
+counterA.increment()
+counterA.increment()
+counterA.increment()
+
+counterB.decrement()
+counterB.decrement()
