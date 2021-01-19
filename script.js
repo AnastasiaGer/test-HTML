0204bbaf6da6b4d0e67f78a50111c5cb5b3bbd55
AnastasiaGer <aagermanova@gmail.com>
2021-01-18 19:18:34 -0800

    Синтаксис с обратными ковычками `

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-const form = document.querySelector('form')
-
-form.addEventListener('submit', event => {
-
-  event.preventDefault()
-
-  const title = form.title.value
-  const text = form.text.value
-  const description = form.description.value
-
-  saveForm(title, text, description)
-
-})
-
-// Spread
-// function saveForm(data) {
-//   const formData = {
-//     date: new Date().toLocaleDateString(),
-//     ...data
-//   }
-
-//   console.log('Ford data:', formData)
-// }
-
-// Rest
-function saveForm(...args) {
-
-  const [title, text, description] = args
-
-  const formData = {
-    date: new Date().toLocaleDateString(),
-    title, text, description
-  }
-
-  console.log('Ford data:', formData)
+const createLink = ({path, name}) => {
+  return `<a target="_blank" href="${path}">${name}</a>`
 }
+const ul = document.querySelector('ul')
+const google = `<li>${createLink({path: 'https://www.google.com/', name: 'Google'})}</li>`
+const yandex = `<li>${createLink({path: 'https://www.ya.ru/', name: 'Yandex'})}</li>`
+ul.insertAdjacentHTML('afterbegin', google)
+ul.insertAdjacentHTML('afterbegin', yandex)
