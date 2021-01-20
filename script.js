28a1f586f71bf47a53d61cba8b0fe60d49e0d4a5
AnastasiaGer <aagermanova@gmail.com>
2021-01-19 16:23:27 -0800

    async

--- script.js
+++ script.js
@@ -1,67 +1,14 @@
-class Dropdown {
-  constructor(selector, options) {
-    this.$el = document.querySelector(selector)
-    this.items = options.items
+// setTimeout(function() {
+//   alert('Hello timeout')
+// }, 2 * 1000)
 
-    this.$el.querySelector('.dropdown__label').textContent = this.items[0].label
+var counter = 0
 
-    this.$el.addEventListener('click', event => {
-      if (event.target.classList.contains('dropdown__label')) {
-        if (this.$el.classList.contains('open')) {
-          this.close()
-        } else {
-          this.open()
-        }
-      } else if (event.target.tagName.toLowerCase() === 'li') {
-        this.select(event.target.dataset.id)
-      }
-    })
+var interval = setInterval(function() {
+  console.log(++counter)
+}, 1000 * 2)
 
-    const itemsHTML = this.items.map(i => {
-      return `<li data-id="${i.id}">${i.label}</li>`
-    }).join(' ')
+setTimeout(function() {
+  clearInterval(interval)
+}, 5 * 1000)
 
-    this.$el.querySelector('.dropdown__menu').insertAdjacentHTML('afterbegin', itemsHTML)
-  }
-
-  select(id) {
-    const item = this.items.find(i => i.id === id)
-    this.$el.querySelector('.dropdown__label').textContent = item.label
-    this.close()
-  }
-
-  open() {
-    this.$el.classList.add('open')
-  }
-
-  close() {
-    this.$el.classList.remove('open')
-  }
-}
-
-
-
-
-const dropdown = new Dropdown('#dropdown', {
-  items: [{
-      label: 'Москва',
-      id: 'msk'
-    },
-    {
-      label: 'Санкт-Петербург',
-      id: 'spb'
-    },
-    {
-      label: 'Новосибирск',
-      id: 'nsk'
-    },
-    {
-      label: 'Краснодар',
-      id: 'krdr'
-    },
-    {
-      label: 'Ростов',
-      id: 'rostov'
-    }
-  ]
-})
