c3bc3c052dd3b10a0f1b129dcdb1c0e39c1abc04
AnastasiaGer <aagermanova@gmail.com>
2021-01-20 13:56:47 -0800

    async - await

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const loadButton = document.querySelector('#load').addEventListener('click', load)
 
-function load() {
+async function load() {
+  // var url = 'https://jsonplaceholder.typicode.com/users'
+  // fetch(url)
+  //   .then(function (response) {
+  //     return response.json()
+  //   })
+  //   .then(function (data) {
+  //     let ul = document.querySelector('#list')
+  //     let html = data.map(function (item) {
+  //       return `<li>${item.id}  ${item.name} '${item.email}'</li>`
+  //     })
+
+  //     ul.insertAdjacentHTML('afterbegin', html.join(' '))
+
+  //   })
   var url = 'https://jsonplaceholder.typicode.com/users'
-  fetch(url)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (data) {
-      let ul = document.querySelector('#list')
+  let response = await fetch(url)
+  let data = await response.json()
+
+  let ul = document.querySelector('#list')
       let html = data.map(function (item) {
         return `<li>${item.id}  ${item.name} '${item.email}'</li>`
       })
 
       ul.insertAdjacentHTML('afterbegin', html.join(' '))
-
-    })
 }
