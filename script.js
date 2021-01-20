34ce8f6badc6abb4993a76d7da2df759c52dc4c2
AnastasiaGer <aagermanova@gmail.com>
2021-01-20 14:05:25 -0800

    promis

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-const loadButton = document.querySelector('#load').addEventListener('click', load)
-
-async function load() {
-  // var url = 'https://jsonplaceholder.typicode.com/users'
-  // fetch(url)
-  //   .then(function (response) {
-  //     return response.json()
-  //   })
-  //   .then(function (data) {
-  //     let ul = document.querySelector('#list')
-  //     let html = data.map(function (item) {
-  //       return `<li>${item.id}  ${item.name} '${item.email}'</li>`
-  //     })
-
-  //     ul.insertAdjacentHTML('afterbegin', html.join(' '))
-
-  //   })
-  var url = 'https://jsonplaceholder.typicode.com/users'
-  let response = await fetch(url)
-  let data = await response.json()
-
-  let ul = document.querySelector('#list')
-      let html = data.map(function (item) {
-        return `<li>${item.id}  ${item.name} '${item.email}'</li>`
-      })
-
-      ul.insertAdjacentHTML('afterbegin', html.join(' '))
+function sleep(ms) {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
+      resolve()
+    }, ms)
+  })
 }
+
+var p1 = sleep(1500).then(function() {
+  return {
+    name: 'Promise 1500'
+  }
+})
+
+var p2 = sleep(3000).then(function() {
+  return {
+    name: 'Promise 3000'
+  }
+})
+
+var p3 = sleep(4000).then(function() {
+  return {
+    name: 'Promise 4000'
+  }
+})
+
+async function start() {
+  var dataAll = await Promise.all([p1, p2, p3])
+
+  var dataRace = await Promise.race([p1, p2])
+
+  console.log('dataAll', dataAll)
+  console.log('dataRace', dataRace)
+}
+
+start()
+
